3e32544ced0025f28ed8b85ad2857bb76aeb1654
kevino430 <ruby004949@gmail.com>
2024-04-12 15:02:17 +0800

    試試看 ezewb360.xyz

--- api_test.php
+++ api_test.php
@@ -65,3 +65,54 @@ echo "</pre>";
 echo "Hello World";
 
 // test 002
+echo "<hr\n>";
+echo "<pre>";
+print_r("我要測試 打 https://ezweb360.xyz \n");
+echo "</pre>";
+
+echo "<pre>";
+print_r("以下是我 https://ezweb360.xyz 的測試 \n");
+echo "</pre>";
+
+$curl = curl_init();
+
+curl_setopt($curl, CURLINFO_HEADER_OUT, true);
+
+curl_setopt_array($curl, array(
+  
+  CURLOPT_URL => 'https://ezweb360.xyz',
+  
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'GET',
+  CURLOPT_HTTPHEADER => array(
+    'Content-Type: text/plain'
+  ),
+));
+
+$response = curl_exec($curl);
+$status_code = curl_getinfo($curl, CURLINFO_HTTP_CODE); // 获取 HTTP 状态码
+
+// 获取请求头部
+$requestHeaders = curl_getinfo($curl, CURLINFO_HEADER_OUT);
+echo "<pre>Request Headers: \n";
+echo print_r($requestHeaders);
+echo "</pre>";
+
+
+if (curl_errno($curl)) {
+    // 如果请求出错，显示错误信息
+    print_r("cURL Error: " . htmlspecialchars(curl_error($curl)));
+} else {
+    // 显示响应状态码
+    echo "HTTP Status Code: " . $status_code . "\n";
+    if ($status_code == 403) {
+        print_r("就是403 啥都沒回..... 這是我自記寫的 是因為 curl_errno 沒東西才寫。 (無法訪問 API。這可能是由於缺少或無效的憑證、IP地址被封鎖、伺服器配置錯誤，或觸發了安全規則。)\n");
+    }
+}
+
+curl_close($curl);
